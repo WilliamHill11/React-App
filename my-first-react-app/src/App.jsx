860666e80e18ca3c6c5112d7fc563ab37ed668f5
william hill <williamhill0831@Hotmail.com>
2023-09-23 15:02:07 -0400

    Working with JSX

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -25,11 +25,9 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Hello WORLD! YESSSIAA</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ my-first-react-app/src/Greeting.jsx
@@ -0,0 +1,16 @@
+function Greeting() {
+  return (
+    <div>
+      <h1>&quot;How r u baby&quot;</h1>
+      <h2>GoodMorning</h2>;
+    </div>
+  );
+}
+
+export function Cranberry() {
+  return <h3>Have a day!!</h3>;
+}
+
+Cranberry();
+
+export default Greeting;
