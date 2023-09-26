8f656e33077d447e8f8da983c650a7fa525b4d3b
william hill <williamhill0831@Hotmail.com>
2023-09-25 23:46:25 -0400

    more rendering techniques

--- my-first-react-app/src/Greeting.jsx
+++ my-first-react-app/src/Greeting.jsx
@@ -16,22 +16,72 @@
 
 // export default Greeting;
 
-function ListItem(props) {
-  return <li>{props.animal}</li>;
-}
+// function ListItem(props) {
+//   return <li>{props.animal}</li>;
+// }
+
+// function List(props) {
+//   return (
+//     <ul>
+//       {props.animals.map((animal) => {
+//         return <ListItem key={animal} animal={animal} />;
+//       })}
+//     </ul>
+//   );
+// }
+
+// function Greeting() {
+//   const animals = ['Lion', 'Cow', 'Snake', 'Lizard'];
+
+//   return (
+//     <div>
+//       <h1>Animals: </h1>
+//       <List animals={animals} />
+//     </div>
+//   );
+// }
+
+// function List(props) {
+//   return (
+//     <ul>
+//       {props.animals.map((animal) => {
+//         return animal.startsWith('L') ? <li key={animal}>{animal}</li> : null;
+//       })}
+//     </ul>
+//   );
+// }
+
+// function Greeting() {
+//   const animals = ['Lion', 'Cow', 'Snake', 'Lizard'];
+
+//   return (
+//     <div>
+//       <h1>Animals: </h1>
+//       <List animals={animals} />
+//     </div>
+//   );
+// }
 
 function List(props) {
+  if (!props.animals) {
+    return <div>Loading...</div>;
+  }
+
+  if (props.animals.length === 0) {
+    return <div>There are no animals in the list!</div>;
+  }
+
   return (
     <ul>
       {props.animals.map((animal) => {
-        return <ListItem key={animal} animal={animal} />;
+        return <li key={animal}>{animal}</li>;
       })}
     </ul>
   );
 }
 
 function Greeting() {
-  const animals = ['Lion', 'Cow', 'Snake', 'Lizard'];
+  const animals = [''];
 
   return (
     <div>
