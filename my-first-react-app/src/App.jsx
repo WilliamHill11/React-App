357d1cbe9c35fd09afbcebb72c55801df0f270d1
william hill <williamhill0831@Hotmail.com>
2023-10-11 12:59:28 -0400

    hide/show using useState

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,32 +1,14 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [showText, SetShowText] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Hello WORLD! YESSSIAA</p>
-    </>
+    <div>
+      <button onClick={() => SetShowText(!showText)}>Hide/Show Text</button>
+      {showText && <h1>This is a text</h1>}
+    </div>
   );
 }
 
