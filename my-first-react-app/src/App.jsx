11ce126b6e9ce2fed35acdc69d16b231fb2c4224
william hill <williamhill0831@Hotmail.com>
2023-10-15 21:26:40 -0400

    working with useEffect Hook

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -58,66 +58,88 @@
 //   );
 // }
 
-import { useState } from 'react';
+// import { useState } from 'react';
+// import './App.css';
+// import Task from './Task';
+
+// function App() {
+//   const [todoList, setTodoList] = useState([]);
+//   const [newTask, setNewTask] = useState('');
+
+//   const handleChange = (event) => {
+//     setNewTask(event.target.value);
+//   };
+
+//   const addTask = () => {
+//     const task = {
+//       id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+//       taskName: newTask,
+//       completed: false,
+//     };
+
+//     setTodoList([...todoList, task]);
+//   };
+
+//   const deleteTask = (id) => {
+//     setTodoList(todoList.filter((newTask) => id !== newTask.id));
+//   };
+
+//   const completeTask = (id) => {
+//     setTodoList(
+//       todoList.map((task) => {
+//         console.log(task, 'task');
+//         if (task.id === id) {
+//           return { ...task, completed: true };
+//         } else {
+//           return task;
+//         }
+//       })
+//     );
+//   };
+
+//   return (
+//     <div className="App">
+//       <div className="addTask">
+//         <input type="text" onChange={handleChange} />
+//         <button onClick={addTask}>Add Task</button>
+//       </div>
+//       <div className="list">
+//         <div>
+//           {todoList.map((task) => {
+//             return (
+//               <Task
+//                 taskName={task.taskName}
+//                 id={task.id}
+//                 deleteTask={deleteTask}
+//                 completeTask={completeTask}
+//                 completed={task.completed}
+//               />
+//             );
+//           })}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+import { useState, useEffect } from 'react';
 import './App.css';
-import Task2 from './Task2';
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [newTask, setNewTask] = useState('');
+  const [catFact, setCatFact] = useState('');
 
-  const handleChange = (event) => {
-    setNewTask(event.target.value);
-  };
-
-  const addTask = () => {
-    const task = {
-      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
-      taskName: newTask,
-      completed: false,
-    };
-
-    setTodoList([...todoList, task]);
-  };
-
-  const deleteTask = (id) => {
-    setTodoList(todoList.filter((newTask) => id !== newTask.id));
-  };
-
-  const completeTask = (id) => {
-    setTodoList(
-      todoList.map((task) => {
-        console.log(task, 'task');
-        if (task.id === id) {
-          return { ...task, completed: true };
-        } else {
-          return task;
-        }
-      })
-    );
-  };
+  useEffect(() => {
+    fetch('https://catfact.ninja/fact')
+      .then((res) => res.json())
+      .then((data) => {
+        setCatFact(data.fact);
+      });
+  }, []);
 
   return (
     <div className="App">
-      <div className="addTask">
-        <input type="text" onChange={handleChange} />
-        <button onClick={addTask}>Add Task</button>
-      </div>
-      <div className="list">
-        <div>
-          {todoList.map((task) => {
-            return (
-              <Task2
-                taskName={task.taskName}
-                id={task.id}
-                deleteTask={deleteTask}
-                completeTask={completeTask}
-                completed={task.completed}
-              />
-            );
-          })}
-        </div>
-      </div>
+      <button>Generate Cat Fact</button>
+      <p> {catFact} </p>
     </div>
   );
 }
