327f1162aba52cd3aef2714e809a74c48d8e8d1a
william hill <williamhill0831@Hotmail.com>
2023-11-01 14:26:19 -0400

    added key variable in useEffect

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -128,9 +128,13 @@ function App() {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
-    setInterval(() => {
+    const key = setInterval(() => {
       setCounter((count) => count + 1);
     }, 1000);
+
+    return () => {
+      clearInterval(key);
+    };
   }, []);
 
   return <p>{counter} seconds have passed.</p>;
