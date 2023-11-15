23d82459ba69ef0333ff5493afaa1f11a4d319ee
william hill <williamhill0831@Hotmail.com>
2023-11-15 12:50:14 -0500

    testing

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -102,6 +102,23 @@
 
 // export default App;
 
-const App = () => <h1>Our First Test</h1>;
+import React, { useState } from 'react';
+
+const App = () => {
+  const [heading, setHeading] = useState('Magnificent Monkeys');
+
+  const clickHandler = () => {
+    setHeading('Radical Rhinos');
+  };
+
+  return (
+    <>
+      <button type="button" onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
+    </>
+  );
+};
 
 export default App;
